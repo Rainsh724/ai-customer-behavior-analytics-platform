203387ffeebca20be3e7f256aa24f2f1399405a8
Zeinab Abedini <Zynbabdyny10@gmail.com>
2026-09-10 12:13:11 +0330

    test

--- Database/index.sql
+++ Database/index.sql
@@ -76,6 +76,96 @@ ANALYZE categories;
 ANALYZE sessions;
 
 
+-- کد بالا برام ارور میداد چون ستون امبدد کامنت توی جدول کامنت بود کوئری پایین معادل بالاییه
+
+
+CREATE INDEX IF NOT EXISTS idx_comments_embedding_hnsw
+ON comments_embedding
+USING hnsw (embedded_comment vector_cosine_ops);
+
+
+CREATE INDEX IF NOT EXISTS idx_comments_product
+ON comments(product_id);
+
+CREATE INDEX IF NOT EXISTS idx_comments_created_at
+ON comments(created_at);
+
+CREATE INDEX IF NOT EXISTS idx_comments_product_date
+ON comments(product_id, created_at);
+
+CREATE INDEX IF NOT EXISTS idx_comments_time_id
+ON comments(created_at, id);
+
+CREATE INDEX IF NOT EXISTS idx_comments_recommendation
+ON comments(recommendation_status);
+
+CREATE INDEX IF NOT EXISTS idx_behavior_event_product
+ON user_behavior_logs (event_type, product_id);
+
+CREATE INDEX IF NOT EXISTS idx_behavior_product_event
+ON user_behavior_logs (product_id, event_type);
+
+CREATE INDEX IF NOT EXISTS idx_behavior_event_time
+ON user_behavior_logs (event_type, timestamp);
+
+CREATE INDEX IF NOT EXISTS idx_behavior_user_time
+ON user_behavior_logs (session_id, timestamp);
+
+CREATE INDEX IF NOT EXISTS idx_behavior_product_time
+ON user_behavior_logs (product_id, timestamp);
+
+CREATE INDEX IF NOT EXISTS idx_behavior_purchase_time
+ON user_behavior_logs (event_type, timestamp, product_id);
+
+
+CREATE INDEX IF NOT EXISTS idx_products_category
+ON products(category_id);
+
+CREATE INDEX IF NOT EXISTS idx_products_category_brand
+ON products(category_id, brand_id);
+
+CREATE INDEX IF NOT EXISTS idx_products_brand_category
+ON products(brand_id, category_id);
+
+
+CREATE INDEX IF NOT EXISTS idx_sessions_user_city
+ON sessions(user_id, city_id);
+
+
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'comment_aspects'
+ORDER BY ordinal_position;
+
+CREATE TABLE product_negative_feedback_summary AS
+SELECT c.product_id,
+       AVG(ca.negative_pct) AS avg_negative_pct,
+       COUNT(DISTINCT c.id) AS comment_cnt
+FROM comments c
+JOIN comment_aspects ca
+    ON ca.comment_id = c.id
+GROUP BY c.product_id;
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_product_negative_feedback_product
+ON product_negative_feedback_summary(product_id);
+
+
+ANALYZE product_negative_feedback_summary;
+
+
+ANALYZE user_behavior_logs;
+ANALYZE comments;
+ANALYZE comments_embedding;
+ANALYZE comment_aspects;
+ANALYZE products;
+ANALYZE categories;
+ANALYZE sessions;
+
+
+
+
+
+
 
 --- جدول جدید برای دقت و معیارهای ارزیابی چت ها و عملکرد Llm
 
